c1db71be437660abc7037b8674dc37d1f3443ecf
NakulShinde <nakulshinde.it@gmail.com>
2017-09-29 15:38:28 +0530

    UserApp dashboard initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
+  	AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],
